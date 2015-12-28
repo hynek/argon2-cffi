6aeb19268ac50f7234eb27d1a50d4cd762ef19dd
Bas Westerbaan <bas@westerbaan.name>
2015-12-28 16:43:53 +0100

    low-level: replace unicode character

--- src/argon2/low_level.py
+++ src/argon2/low_level.py
@@ -4,7 +4,7 @@ Low-level functions if you want to build your own higher level abstractions.
 
 .. warning::
     This is a "Hazardous Materials" module.  You should **ONLY** use it if
-    you’re 100% absolutely sure that you know what you’re doing because this
+    you're 100% absolutely sure that you know what you’re doing because this
     module is full of land mines, dragons, and dinosaurs with laser guns.
 """
 
