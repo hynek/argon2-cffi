37b1cf6f681727ec5613f26dbd1c2f4c81e57f08
Hynek Schlawack <hs@ox.cx>
2016-11-05 13:48:29 +0100

    Avoid cffi build process if not necessary
    fixes #17

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from __future__ import absolute_import, division, print_function
 
 from distutils.command.build_clib import build_clib
+from distutils.command.build import build
 from distutils.errors import DistutilsSetupError
 
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 
 import sys
 import codecs
@@ -96,6 +98,132 @@ if sys.version_info[0:2] < (3, 4):
 EXTRAS_REQUIRE = {}
 
 ###############################################################################
+
+
+def keywords_with_side_effects(argv):
+    """
+    Get a dictionary with setup keywords that (can) have side effects.
+
+    :param argv: A list of strings with command line arguments.
+
+    :returns: A dictionary with keyword arguments for the ``setup()`` function.
+        This setup.py script uses the setuptools 'setup_requires' feature
+        because this is required by the cffi package to compile extension
+        modules. The purpose of ``keywords_with_side_effects()`` is to avoid
+        triggering the cffi build process as a result of setup.py invocations
+        that don't need the cffi module to be built (setup.py serves the dual
+        purpose of exposing package metadata).
+
+    Stolen from pyca/cryptography.
+    """
+    no_setup_requires_arguments = (
+        '-h', '--help',
+        '-n', '--dry-run',
+        '-q', '--quiet',
+        '-v', '--verbose',
+        '-V', '--version',
+        '--author',
+        '--author-email',
+        '--classifiers',
+        '--contact',
+        '--contact-email',
+        '--description',
+        '--egg-base',
+        '--fullname',
+        '--help-commands',
+        '--keywords',
+        '--licence',
+        '--license',
+        '--long-description',
+        '--maintainer',
+        '--maintainer-email',
+        '--name',
+        '--no-user-cfg',
+        '--obsoletes',
+        '--platforms',
+        '--provides',
+        '--requires',
+        '--url',
+        'clean',
+        'egg_info',
+        'register',
+        'sdist',
+        'upload',
+    )
+
+    def is_short_option(argument):
+        """Check whether a command line argument is a short option."""
+        return len(argument) >= 2 and argument[0] == '-' and argument[1] != '-'
+
+    def expand_short_options(argument):
+        """Expand combined short options into canonical short options."""
+        return ('-' + char for char in argument[1:])
+
+    def argument_without_setup_requirements(argv, i):
+        """Check whether a command line argument needs setup requirements."""
+        if argv[i] in no_setup_requires_arguments:
+            # Simple case: An argument which is either an option or a command
+            # which doesn't need setup requirements.
+            return True
+        elif (is_short_option(argv[i]) and
+              all(option in no_setup_requires_arguments
+                  for option in expand_short_options(argv[i]))):
+            # Not so simple case: Combined short options none of which need
+            # setup requirements.
+            return True
+        elif argv[i - 1:i] == ['--egg-base']:
+            # Tricky case: --egg-info takes an argument which should not make
+            # us use setup_requires (defeating the purpose of this code).
+            return True
+        else:
+            return False
+
+    if all(argument_without_setup_requirements(argv, i)
+           for i in range(1, len(argv))):
+        return {
+            "cmdclass": {
+                "build": DummyBuild,
+                "install": DummyInstall,
+            }
+        }
+    else:
+        return {
+            "setup_requires": SETUP_REQUIRES,
+            "cffi_modules": CFFI_MODULES,
+            "libraries": LIBRARIES,
+            "cmdclass": {
+                "build_clib": BuildCLibWithCompilerFlags,
+            },
+        }
+
+
+setup_requires_error = (
+    "Requested setup command that needs 'setup_requires' while command line "
+    "arguments implied a side effect free command or option."
+)
+
+
+class DummyBuild(build):
+    """
+    This class makes it very obvious when ``keywords_with_side_effects()`` has
+    incorrectly interpreted the command line arguments to ``setup.py build`` as
+    one of the 'side effect free' commands or options.
+    """
+
+    def run(self):
+        raise RuntimeError(setup_requires_error)
+
+
+class DummyInstall(install):
+    """
+    This class makes it very obvious when ``keywords_with_side_effects()`` has
+    incorrectly interpreted the command line arguments to ``setup.py install``
+    as one of the 'side effect free' commands or options.
+    """
+
+    def run(self):
+        raise RuntimeError(setup_requires_error)
+
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
@@ -195,13 +323,12 @@ if __name__ == "__main__":
         keywords=KEYWORDS,
         packages=PACKAGES,
         package_dir={"": "src"},
-        cffi_modules=CFFI_MODULES,
-        ext_package="argon2",
-        libraries=LIBRARIES,
-        cmdclass={"build_clib": BuildCLibWithCompilerFlags},
-        zip_safe=False,
         classifiers=CLASSIFIERS,
-        setup_requires=SETUP_REQUIRES,
         install_requires=INSTALL_REQUIRES,
         extras_require=EXTRAS_REQUIRE,
+
+        # CFFI
+        zip_safe=False,
+        ext_package="argon2",
+        **keywords_with_side_effects(sys.argv)
     )
