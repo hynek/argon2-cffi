1136bd9b1fd459cf2ffd39aea7935bd033fce60a
Kubilay Kocak <koobs@users.noreply.github.com>
2019-06-03 04:46:21 +0000

    Disable deadline on test_argument_ranges
    test_argument_ranges fails intermittently (with high probability) with the
    following error:
    
      Falsifying example: test_argument_ranges(password='', time_cost=1, parallelism=4, memory_cost=740, hash_len=196, salt_len=223)
    
      Unreliable test timings! On an initial run, this test took 332.92ms, which
      exceeded the deadline of 200.00ms, but on a subsequent run it took 24.19
      ms, which did not. If you expect this sort of variability in your test
      timings, consider turning deadlines off for this test by setting
      deadline=None.
    
    Given the test is slow "This test is rather slow." and test can be run in
    a multitude of environments including virtualized environments, and test
    timing doesn't appear to be relevant or valuable, disable deadlines for
    this test accordingly.

--- tests/test_low_level.py
+++ tests/test_low_level.py
@@ -5,7 +5,7 @@ import os
 
 import pytest
 
-from hypothesis import assume, given
+from hypothesis import assume, given, settings
 from hypothesis import strategies as st
 
 from argon2.exceptions import (
@@ -253,6 +253,7 @@ class TestVerify(object):
     hash_len=st.integers(lib.ARGON2_MIN_OUTLEN, 513),
     salt_len=st.integers(lib.ARGON2_MIN_SALT_LENGTH, 513),
 )
+@settings(deadline=None)
 def test_argument_ranges(
     password, time_cost, parallelism, memory_cost, hash_len, salt_len
 ):
